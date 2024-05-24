b43ef0a0970ed139f3361d1da9e8a96b581bc9b3
Juliette Pelletier <juliette.pel@hotmail.com>
2024-05-24 10:53:01 -0400

    Level WIP + Ingredient spawner

---
+++ Assets/Scripts/Gameplay/IngredientSpawner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameObject ingredientPrefab;
+
+    private Ingredient _currentIngredient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_currentIngredient == null) 
+        {
+            SpawnIngredient();
+        }
+    }
+
+    void SpawnIngredient()
+    {
+        _currentIngredient = Instantiate(ingredientPrefab, spawnPoint.position, spawnPoint.rotation).GetComponent<Ingredient>();
+        _currentIngredient.Spawner = this;
+    }
+
+    public void OnIngredientPickedUp()
+    {
+        _currentIngredient = null;
+    }
+}
